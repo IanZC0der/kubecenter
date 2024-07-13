3e04b2d748df8203340ce83fca9492ea08eca422
Ian Zhang <benchi2@pdx.edu>
2024-07-12 19:15:26 -0500

    impl secret service interface

---
+++ util/util.go
@@ -0,0 +1,30 @@
+package util
+
+type ListItem struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
+
+func MapConverter(data []*ListItem) map[string]string {
+	dataMap := make(map[string]string)
+	for _, data := range data {
+		dataMap[data.Key] = data.Value
+	}
+	return dataMap
+}
+
+func ListConverter(data map[string]string) []*ListItem {
+	l := make([]*ListItem, 0)
+	for k, v := range data {
+		l = append(l, &ListItem{Key: k, Value: v})
+	}
+	return l
+}
+
+func ListConverterFromByte(data map[string][]byte) []*ListItem {
+	l := make([]*ListItem, 0)
+	for k, v := range data {
+		l = append(l, &ListItem{Key: k, Value: string(v)})
+	}
+	return l
+}
