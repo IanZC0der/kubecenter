fd2d8e94b08c2ce861d5d8523b23ce6db007858e
Ian Zhang <benchi2@pdx.edu>
2024-07-16 13:06:22 -0500

    add persistent volume claim api, pass unit test and api test

--- apps/persistentvolume/interface.go
+++ apps/persistentvolume/interface.go
@@ -13,4 +13,7 @@ type Service interface {
 	GetPVList(context.Context, string) ([]*corev1.PersistentVolume, error)
 	DeletePV(context.Context, string) error
 	CreatePV(context.Context, *PersistentVolumeReq) (*corev1.PersistentVolume, error)
+	GetPVCList(context.Context, string, string) ([]*corev1.PersistentVolumeClaim, error)
+	DeletePVC(context.Context, string, string) error
+	CreatePVC(context.Context, *PersistentVolumeClaim) (*corev1.PersistentVolumeClaim, error)
 }
