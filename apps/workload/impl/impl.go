5e42bbe67c79999e89f9294e3dd71ea236364113
Ian Zhang <benchi2@pdx.edu>
2024-07-18 13:25:59 -0500

    impl create/update/delete stateful set

--- apps/workload/impl/impl.go
+++ apps/workload/impl/impl.go
@@ -2,14 +2,18 @@ package impl
 
 import (
 	"context"
+	"fmt"
 	"github.com/IanZC0der/kubecenter/apps/persistentvolume"
 	"github.com/IanZC0der/kubecenter/apps/pods"
 	"github.com/IanZC0der/kubecenter/apps/workload"
 	"github.com/IanZC0der/kubecenter/global"
 	"github.com/IanZC0der/kubecenter/ioc"
 	"github.com/IanZC0der/kubecenter/util"
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"strconv"
 	"strings"
 )
 
@@ -88,9 +92,77 @@ func (w *WorkloadServiceImpl) GetStatefulSetList(ctx context.Context, namespace,
 
 	return res, nil
 }
-func (w *WorkloadServiceImpl) CreateOrUpdateStatefulSet(ctx context.Context, req *workload.StatefulSet) (*workload.StatefulSetResponse, error) {
-	return nil, nil
+func (w *WorkloadServiceImpl) CreateOrUpdateStatefulSet(ctx context.Context, req *workload.StatefulSet) (*workload.StatefulSetResponse, string, error) {
+
+	pvcTemplates := make([]corev1.PersistentVolumeClaim, len(req.VolumeClaimTemplates))
+
+	for i, item := range req.VolumeClaimTemplates {
+		pvcTemplates[i] = corev1.PersistentVolumeClaim{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:   item.Name,
+				Labels: util.MapConverter(item.Labels),
+			},
+			Spec: corev1.PersistentVolumeClaimSpec{
+				AccessModes: item.AccessModes,
+				Resources: corev1.ResourceRequirements{
+					Requests: corev1.ResourceList{
+						corev1.ResourceStorage: resource.MustParse(strconv.Itoa(int(item.Capacity)) + "Mi"),
+					},
+				},
+			},
+		}
+	}
+	k8sPod := pods.CreatePodFromPodRequest(req.Template)
+	statefulserK8S := &appsv1.StatefulSet{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      req.Name,
+			Namespace: req.Namespace,
+			Labels:    util.MapConverter(req.Labels),
+		},
+		Spec: appsv1.StatefulSetSpec{
+			Replicas:    &req.Replicas,
+			ServiceName: req.ServiceName,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: util.MapConverter(req.Selector),
+			},
+			VolumeClaimTemplates: pvcTemplates,
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: k8sPod.ObjectMeta,
+				Spec:       k8sPod.Spec,
+			},
+		},
+	}
+	existing, err := global.KubeConfigSet.AppsV1().StatefulSets(req.Namespace).Get(ctx, req.Name, metav1.GetOptions{})
+	msg := ""
+	var res *appsv1.StatefulSet
+	if err != nil {
+		//create
+		res, err = global.KubeConfigSet.AppsV1().StatefulSets(req.Namespace).Create(ctx, statefulserK8S, metav1.CreateOptions{})
+		if err != nil {
+			msg = fmt.Sprintf("Create statefulset err: %s", err.Error())
+			return nil, msg, err
+		} else {
+			msg = fmt.Sprintf("Create statefulset success: %s", res.Name)
+		}
+	} else {
+		existing.Spec = statefulserK8S.Spec
+		res, err = global.KubeConfigSet.AppsV1().StatefulSets(req.Namespace).Update(ctx, existing, metav1.UpdateOptions{})
+		if err != nil {
+			msg = fmt.Sprintf("Update statefulset err: %s", err.Error())
+			return nil, msg, err
+		} else {
+			msg = fmt.Sprintf("Update statefulset success: %s", res.Name)
+		}
+	}
+	workloadResp := &workload.StatefulSetResponse{
+		Name:      res.Name,
+		Namespace: res.Namespace,
+		Replicas:  res.Status.Replicas,
+		Ready:     res.Status.ReadyReplicas,
+		Age:       res.CreationTimestamp.Unix(),
+	}
+	return workloadResp, msg, nil
 }
 func (w *WorkloadServiceImpl) DeleteStatefulSet(ctx context.Context, namespace, name string) error {
-	return nil
+	return global.KubeConfigSet.AppsV1().StatefulSets(namespace).Delete(ctx, name, metav1.DeleteOptions{})
 }
--- apps/workload/interface.go
+++ apps/workload/interface.go
@@ -9,6 +9,6 @@ const (
 type Service interface {
 	GetStatefulSetDetail(ctx context.Context, namespace, name string) (*StatefulSet, error)
 	GetStatefulSetList(ctx context.Context, namespace, keyword string) ([]*StatefulSetResponse, error)
-	CreateOrUpdateStatefulSet(ctx context.Context, req *StatefulSet) (*StatefulSetResponse, error)
+	CreateOrUpdateStatefulSet(ctx context.Context, req *StatefulSet) (*StatefulSetResponse, string, error)
 	DeleteStatefulSet(ctx context.Context, namespace, name string) error
 }
